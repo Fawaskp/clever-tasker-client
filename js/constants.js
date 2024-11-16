dbedc958856d40b7eb7ae6db2abcb4abcb550ec4
fawaskp <fawaskp1010@gmail.com>
2024-11-16 18:52:38 +0530

    refactor: server url updated with new

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-const API_BASE_URL = isLocal ? 'http://localhost:8000' : 'http://43.205.236.147/';
+const API_BASE_URL = isLocal ? 'http://localhost:8000' : 'https://clevertasker.keeowellness.in';
 const AUTH_API_URL = `${API_BASE_URL}/api/auth`
 const TASK_API_URL = `${API_BASE_URL}/api/tasks`
